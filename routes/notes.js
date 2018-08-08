9775851b9f844239b588f1328da9357d7be3ff58
kathyleboeuf <kleboeuf@townisp.com>
2018-08-07 20:01:22 -0400

    first commit Knex queries work

--- routes/notes.js
+++ routes/notes.js
@@ -1,22 +1,33 @@
 'use strict';
 
 const express = require('express');
-
+const knex = require('../knex');
 // Create an router instance (aka "mini-app")
 const router = express.Router();
 
 // TEMP: Simple In-Memory Database
 const data = require('../db/notes');
-const simDB = require('../db/simDB');
-const notes = simDB.initialize(data);
+// const simDB = require('../db/simDB');
+// const notes = simDB.initialize(data);
 
 // Get All (and search by query)
 router.get('/', (req, res, next) => {
   const { searchTerm } = req.query;
 
-  notes.filter(searchTerm)
-    .then(list => {
-      res.json(list);
+  //let searchTerm = '%cat%';
+  knex
+    .select('notes.id', 'title', 'content')
+    .from('notes')
+    .modify(function (queryBuilder) {
+      console.log(searchTerm)
+      if (searchTerm) {
+        queryBuilder.where('title', 'like', `%${searchTerm}%`);
+      }
+    })
+    .orderBy('notes.id')
+    .then(results => {
+      //console.log(results);
+      res.json(results);
     })
     .catch(err => {
       next(err);
@@ -24,90 +35,90 @@ router.get('/', (req, res, next) => {
 });
 
 // Get a single item
-router.get('/:id', (req, res, next) => {
-  const id = req.params.id;
+// router.get('/:id', (req, res, next) => {
+//   const id = req.params.id;
 
-  notes.find(id)
-    .then(item => {
-      if (item) {
-        res.json(item);
-      } else {
-        next();
-      }
-    })
-    .catch(err => {
-      next(err);
-    });
-});
+//   notes.find(id)
+//     .then(item => {
+//       if (item) {
+//         res.json(item);
+//       } else {
+//         next();
+//       }
+//     })
+//     .catch(err => {
+//       next(err);
+//     });
+// });
 
 // Put update an item
-router.put('/:id', (req, res, next) => {
-  const id = req.params.id;
+// router.put('/:id', (req, res, next) => {
+//   const id = req.params.id;
+
+//   /***** Never trust users - validate input *****/
+//   const updateObj = {};
+//   const updateableFields = ['title', 'content'];
+
+//   updateableFields.forEach(field => {
+//     if (field in req.body) {
+//       updateObj[field] = req.body[field];
+//     }
+//   });
 
   /***** Never trust users - validate input *****/
-  const updateObj = {};
-  const updateableFields = ['title', 'content'];
+//   if (!updateObj.title) {
+//     const err = new Error('Missing `title` in request body');
+//     err.status = 400;
+//     return next(err);
+//   }
 
-  updateableFields.forEach(field => {
-    if (field in req.body) {
-      updateObj[field] = req.body[field];
-    }
-  });
-
-  /***** Never trust users - validate input *****/
-  if (!updateObj.title) {
-    const err = new Error('Missing `title` in request body');
-    err.status = 400;
-    return next(err);
-  }
-
-  notes.update(id, updateObj)
-    .then(item => {
-      if (item) {
-        res.json(item);
-      } else {
-        next();
-      }
-    })
-    .catch(err => {
-      next(err);
-    });
-});
+//   notes.update(id, updateObj)
+//     .then(item => {
+//       if (item) {
+//         res.json(item);
+//       } else {
+//         next();
+//       }
+//     })
+//     .catch(err => {
+//       next(err);
+//     });
+// });
 
 // Post (insert) an item
-router.post('/', (req, res, next) => {
-  const { title, content } = req.body;
+// router.post('/', (req, res, next) => {
+//   const { title, content } = req.body;
 
-  const newItem = { title, content };
-  /***** Never trust users - validate input *****/
-  if (!newItem.title) {
-    const err = new Error('Missing `title` in request body');
-    err.status = 400;
-    return next(err);
-  }
+//   const newItem = { title, content };
+//   /***** Never trust users - validate input *****/
+//   if (!newItem.title) {
+//     const err = new Error('Missing `title` in request body');
+//     err.status = 400;
+//     return next(err);
+//   }
 
-  notes.create(newItem)
-    .then(item => {
-      if (item) {
-        res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
-      }
-    })
-    .catch(err => {
-      next(err);
-    });
-});
+//   notes.create(newItem)
+//     .then(item => {
+//       if (item) {
+//         res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+//       }
+//     })
+//     .catch(err => {
+//       next(err);
+//     });
+// });
 
-// Delete an item
-router.delete('/:id', (req, res, next) => {
-  const id = req.params.id;
+// // Delete an item
+// router.delete('/:id', (req, res, next) => {
+//   const id = req.params.id;
 
-  notes.delete(id)
-    .then(() => {
-      res.sendStatus(204);
-    })
-    .catch(err => {
-      next(err);
-    });
-});
+//   notes.delete(id)
+//     .then(() => {
+//       res.sendStatus(204);
+//     })
+//     .catch(err => {
+//       next(err);
+//     });
+// });
 
 module.exports = router;
